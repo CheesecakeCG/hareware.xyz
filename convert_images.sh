0b2fd32f5779ba64cbb099aacacd08fd5d8ba236
hare_ware <3469405-hare_ware@users.noreply.gitlab.com>
2026-03-05 22:18:31 -0500

    Added stuff for furality ultra 2026, mostly css & js features

--- convert_images.sh
+++ convert_images.sh
@@ -2,6 +2,7 @@
 for i in *.png;
   do name=`echo "$i" | cut -d'.' -f1`
   echo "$name"
-  convert "$i" "${name}.jpg"
+  ffmpeg -i  "$i" -c:v libwebp -pix_fmt yuv420p -quality 94 "${name}.webp"
 done
+
 # rm *.png
